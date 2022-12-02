deec59bbd8d93f6520e512f15419a6ec705f3667
jacek <poliocertes@gmail.com>
2022-12-02 16:53:01 +0100

    02.12.2022-3

--- KiK/main.py
+++ KiK/main.py
@@ -1,23 +1,9 @@
-import pygame
-import sys
+from tkinter import *
 
+window = Tk()
+window.title("Kółko i krzyzyk")
+window.geometry('600x600+500+300')
+window.configure(bg="blue")
 
-height = 800
-width = 800
-pygame.init()
-pygame.display.init()
-window = pygame.display.set_mode([width, height])
+mainloop()
 
-pygame.display.update()
-
-
-def main():
-	running = True
-	while running:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				sys.exit().pygame.quit()
-
-
-if __name__ == "__main__":
-	main()
