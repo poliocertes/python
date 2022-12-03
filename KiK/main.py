a2f5dd1c1406a83db2051529d66969890227a4b3
jacek <poliocertes@gmail.com>
2022-12-03 17:55:32 +0100

    3.12.2022-7

--- KiK/main.py
+++ KiK/main.py
@@ -9,9 +9,7 @@ window.configure(bg="blue")
 buttons_area = Frame(window, height=600)
 button_font = tkFont.Font(family='Arial Bold', size=8, weight=tkFont.BOLD)
 color = "#380A2E"
-color_2 = '#8a2be2'
-scores = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']]
-click_counter = 1
+color_2 = '#8a2be2' 
 
 
 def interface():
@@ -28,35 +26,18 @@ def interface():
 	button9 = Button(grid, text="", height= 15, width=30, bg=color, activebackground=color_2, fg='white', font=button_font, command=mark).grid(row="3", column="3", padx=1, pady=1)
 	grid.pack()
 
-
-def draw_symbol():
-	choice = random.randint(0, 1)
-	return choice
-
-
-def mark():
-	global click_counter
-	if click_counter == 1:
-		click_counter += 1
-	else:
-		if click_counter % 2 == 0:
-			print("0")
-			click_counter += 1
-			
-		else:
-			click_counter += 1
-			print("x")
-
-
-def logic():
+def choose_first_symbol():
 	pass
 
+def mark():
+	pass
+
+def check_win():
+	pass
 
 def main():
-	print(draw_symbol())
 	interface()
 	mainloop()
-
 
 if __name__ == "__main__":
 	main()
