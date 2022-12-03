66fe0436fbde51911e2511e5d09107d450f546a5
jacek <poliocertes@gmail.com>
2022-12-03 23:00:20 +0100

    3.12.2022-last

--- KiK/main.py
+++ KiK/main.py
@@ -2,9 +2,27 @@ from tkinter import *
 from tkinter import font as tkFont
 import random
 
-window = Tk()
-window.title("Kółko i krzyżyk")
-window.geometry('670x670+500+300')
-window.configure(bg="blue")
+color="cyan"
+width = 670
+height = 670
+x_dis = 300
+y_dis = 300
+window_location = str(width)+'x'+str(height)+'+'+str(x_dis)+'+'+str(y_dis)
+results_table = [['','',''],['','',''],['','','']]
 
-mainloop()
+def  interface():
+	window = Tk()
+	window.title("Kółko i krzyżyk")
+	window.geometry(window_location)
+	window.configure(bg=color)
+	window.resizable(False, False)
+
+def choose_symbol():
+	return random.randint(0, 1)
+
+def main():
+	interface()
+	mainloop()
+
+if __name__ =="__main__":
+	main()
