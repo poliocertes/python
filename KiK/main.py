9ea6f068b83c4e244a142b06d6647ddf16638fb6
jacek <poliocertes@gmail.com>
2022-12-04 07:52:03 +0100

    4.12.2022-3

--- KiK/main.py
+++ KiK/main.py
@@ -15,16 +15,26 @@ class Game:
 		self.height = height
 		self.color = color
 
+	def board(self):
+		button_font = tkFont.Font(family='Arial Bold', size=8, weight=tkFont.BOLD)
+		Button(grid, text="", height= 5, width=10, bg='blue', activebackground='#0066CC', fg='white', font=button_font, command="").grid(row="1", column="1", padx=10, pady=10)
+		Button(grid, text="", height= 5, width=10, bg='blue', activebackground='#0066CC', fg='white', font=button_font, command="").grid(row="1", column="2", padx=10, pady=10)
+		Button(grid, text="", height= 5, width=10, bg='blue', activebackground='#0066CC', fg='white', font=button_font, command="").grid(row="1", column="3", padx=10, pady=10)
+		Button(grid, text="", height= 5, width=10, bg='blue', activebackground='#0066CC', fg='white', font=button_font, command="").grid(row="2", column="1", padx=10, pady=10)
+		Button(grid, text="", height= 5, width=10, bg='blue', activebackground='#0066CC', fg='white', font=button_font, command="").grid(row="2", column="2", padx=10, pady=10)
+		Button(grid, text="", height= 5, width=10, bg='blue', activebackground='#0066CC', fg='white', font=button_font, command="").grid(row="2", column="3", padx=10, pady=10)
+		Button(grid, text="", height= 5, width=10, bg='blue', activebackground='#0066CC', fg='white', font=button_font, command="").grid(row="3", column="1", padx=10, pady=10)
+		Button(grid, text="", height= 5, width=10, bg='blue', activebackground='#0066CC', fg='white', font=button_font, command="").grid(row="3", column="2", padx=10, pady=10)
+		Button(grid, text="", height= 5, width=10, bg='blue', activebackground='#0066CC', fg='white', font=button_font, command="").grid(row="3", column="3", padx=10, pady=10)
+		
+
 	def  interface(self):
 		window = Tk()
 		window.title("Kółko i krzyżyk")
 		window.geometry(window_size)
-		window.configure(bg=color)
+		window.configure(bg=self.color)
 		window.resizable(False, False)
 		mainloop()
-
-	def board(self):
-		pass
 
 	def choose_first_symbol(self):
 		return random.randint(0, 1)
@@ -33,13 +43,12 @@ class Game:
 		pass
 
 	def check_results(self):
-		if res_table[0][0]==res_table[0][1]==res_table[0][2] or res_table[1][0]==res_table[1][1]==res_table[1][2] or res_table[2][0]==res_table[2][1]==res_table[2][2] or
+		if (res_table[0][0]==res_table[0][1]==res_table[0][2] or res_table[1][0]==res_table[1][1]==res_table[1][2] or res_table[2][0]==res_table[2][1]==res_table[2][2] or
 			res_table[0][0]==res_table[1][0]==res_table[2][0] or res_table[0][1]==res_table[1][1]==res_table[2][1] or res_table[0][2]==res_table[1][2]==res_table[2][2] or
-			res_table[0][0]==res_table[1][1]==res_table[2][2]or res_table[0][2]==res_table[1][1]==res_table[2][0]:
+			res_table[0][0]==res_table[1][1]==res_table[2][2]or res_table[0][2]==res_table[1][1]==res_table[2][0]):
 			pass
 
 def main():
-	runnig = True
 	game = Game()
 	game.interface()
 
