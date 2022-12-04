ce4de32053b72431c9d0d6eca4a499b4337e196f
jacek <poliocertes@gmail.com>
2022-12-04 07:34:35 +0100

    4.12.2022-2

--- KiK/main.py
+++ KiK/main.py
@@ -6,7 +6,7 @@ color="blue"
 width = 670
 height = 670
 window_size = str(width)+'x'+str(height)
-results_table = [['','',''],['','',''],['','','']]
+res_table = [['','',''],['','',''],['','','']]
 
 class Game:
 
@@ -23,14 +23,20 @@ class Game:
 		window.resizable(False, False)
 		mainloop()
 
+	def board(self):
+		pass
+
 	def choose_first_symbol(self):
 		return random.randint(0, 1)
 
-	def mark_button():
+	def mark_button(self):
 		pass
 
-	def check_results():
-		pass
+	def check_results(self):
+		if res_table[0][0]==res_table[0][1]==res_table[0][2] or res_table[1][0]==res_table[1][1]==res_table[1][2] or res_table[2][0]==res_table[2][1]==res_table[2][2] or
+			res_table[0][0]==res_table[1][0]==res_table[2][0] or res_table[0][1]==res_table[1][1]==res_table[2][1] or res_table[0][2]==res_table[1][2]==res_table[2][2] or
+			res_table[0][0]==res_table[1][1]==res_table[2][2]or res_table[0][2]==res_table[1][1]==res_table[2][0]:
+			pass
 
 def main():
 	runnig = True
