cfbbbb198640d25433b76b23513c6d8807f809d4
jacek <poliocertes@gmail.com>
2022-12-05 19:03:44 +0100

    5.12.2022-7

--- KiK/main.py
+++ KiK/main.py
@@ -21,7 +21,10 @@ class Game:
 		return choosen_symbol
 
 	def mark_field(self):
-		print("T")
+		print("X")
+
+	def check_results(self):
+		pass
 
 	def board(self):
 		window = Tk()
@@ -30,33 +33,21 @@ class Game:
 		window.configure(bg=color)
 		window.resizable(False, False)
 		button_font = tkfont.Font(family='Arial Bold', size=8, weight=tkfont.BOLD)
-		button1 = Button(window, text="", height=15, width=30, bg='blue', activebackground='#0066CC', fg='white', font=button_font, command="")
-		button2 = Button(window, text="", height=15, width=30, bg='blue', activebackground='#0066CC', fg='white', font=button_font, command="")
-		button3 = Button(window, text="", height=15, width=30, bg='blue', activebackground='#0066CC', fg='white', font=button_font, command="")
-		button4 = Button(window, text="", height=15, width=30, bg='blue', activebackground='#0066CC', fg='white', font=button_font, command="")
-		button5 = Button(window, text="", height=15, width=30, bg='blue', activebackground='#0066CC', fg='white', font=button_font, command="")
-		button6 = Button(window, text="", height=15, width=30, bg='blue', activebackground='#0066CC', fg='white', font=button_font, command="")
-		button7 = Button(window, text="", height=15, width=30, bg='blue', activebackground='#0066CC', fg='white', font=button_font, command="")
-		button8 = Button(window, text="", height=15, width=30, bg='blue', activebackground='#0066CC', fg='white', font=button_font, command="")
-		button9 = Button(window, text="", height=15, width=30, bg='blue', activebackground='#0066CC', fg='white', font=button_font, command="")
-
-		button1.grid(row=0, column=0, padx=1, pady=1)
-		button2.grid(row=0, column=1, padx=1, pady=1)
-		button3.grid(row=0, column=2, padx=1, pady=1)
-		button4.grid(row=1, column=0, padx=1, pady=1)
-		button5.grid(row=1, column=1, padx=1, pady=1)
-		button6.grid(row=1, column=2, padx=1, pady=1)
-		button7.grid(row=2, column=0, padx=1, pady=1)
-		button8.grid(row=2, column=1, padx=1, pady=1)
-		button9.grid(row=2, column=2, padx=1, pady=1)
-
-	def check_results(self):
-		pass
+		button = Button(window, text="", height=15, width=30, bg='blue', activebackground='#0066CC', fg='white', font=button_font, command=self.mark_field).grid(row=0, column=0, padx=1, pady=1)
+		button = Button(window, text="", height=15, width=30, bg='blue', activebackground='#0066CC', fg='white', font=button_font, command=self.mark_field).grid(row=0, column=1, padx=1, pady=1)
+		button = Button(window, text="", height=15, width=30, bg='blue', activebackground='#0066CC', fg='white', font=button_font, command=self.mark_field).grid(row=0, column=2, padx=1, pady=1)
+		button = Button(window, text="", height=15, width=30, bg='blue', activebackground='#0066CC', fg='white', font=button_font, command=self.mark_field).grid(row=1, column=0, padx=1, pady=1)
+		button = Button(window, text="", height=15, width=30, bg='blue', activebackground='#0066CC', fg='white', font=button_font, command=self.mark_field).grid(row=1, column=1, padx=1, pady=1)
+		button = Button(window, text="", height=15, width=30, bg='blue', activebackground='#0066CC', fg='white', font=button_font, command=self.mark_field).grid(row=1, column=2, padx=1, pady=1)
+		button = Button(window, text="", height=15, width=30, bg='blue', activebackground='#0066CC', fg='white', font=button_font, command=self.mark_field).grid(row=2, column=0, padx=1, pady=1)
+		button = Button(window, text="", height=15, width=30, bg='blue', activebackground='#0066CC', fg='white', font=button_font, command=self.mark_field).grid(row=2, column=1, padx=1, pady=1)
+		button = Button(window, text="", height=15, width=30, bg='blue', activebackground='#0066CC', fg='white', font=button_font, command=self.mark_field).grid(row=2, column=2, padx=1, pady=1)
 
 
 def main():
 	game = Game()
 	game.board()
+
 	mainloop()
 
 
