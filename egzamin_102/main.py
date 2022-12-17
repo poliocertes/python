c13d94af265cc5cd18f9d4bcb162f6f19158a016
jacek <poliocertes@gmail.com>
2022-12-17 07:39:19 +0100

    wersja z komentarzami i nowymi rozwiązaniami

--- egzamin_102/main.py
+++ egzamin_102/main.py
@@ -1,13 +1,29 @@
-# Zadanie na egzamin_102
+# Egzamin_102
 
+class Game:
+	pass
+	''' 
+	Nowa Gra
+	tworzenie nowego gracza
+	lokalizacja Start_room
+	
+	'''
 class Room:
-	def __init__(self, name, location):
+	def __init__(self, name, location, next_room, prev_room, current):
 		self.rooms = ["Hall","Water Room","Fire Room","Sezam"]
 		self.name = name
 		self.location = location
+		self.next_room = next_room
+		self.prev_room = prev_room
+		self.current = current
+		self.items_in_room = []
+
+	'''
+	możliwe opcje. Jak Start_room to nie ma poprzedniego. Idizemy dalej i klikamy co jest i jaki jest cel.
+	'''
 
 	def next_room(self):
-		pass # lokalizacja w tablicy i indeks +1
+		pass
 
 	def previous_room(self):
 		pass # lokalizacja w tablicy i indeks -1
@@ -28,7 +44,13 @@ class Character:
 		self.function = function
 		self.location = location
 
-	def move_to_next(self): # index pokoju. przenosimy postac z listy w daynm pokoju do listy nastepnym (Lub boolean dla kazdego roomu)
+	def get_item(self):
+		pass
+
+	def use_item(self):
+		pass
+
+	def move_to_next(self): # index pokoju. przenosimy postac z listy w danym pokoju do listy nastepnym (Lub boolean dla kazdego roomu)
 		pass
 
 	def move_to_previous(self):
@@ -41,30 +63,30 @@ class Item:
 		self.function = function
 		self.power_impact = power_impact
 
-	def get_item(self):
+	def item_action(self):
 		pass
 
-	def use_item(self):
+	def item_power_impact(self):
 		pass
 
 
-class Hall(Room):
-	def __init__(self, name, location):
-		super().__init__(name, location)
-		pass
+class Start_room(Room):
+	def __init__(self, name, location, next_room, current):
+		super().__init__(name, location, next_room, current)
+		self.next = next
 
 
 class Watter_room(Room):
-	def __init__(self, name, location):
-		super().__init__(name, location)
+	def __init__(self, name, location, next_room, prev_room, current):
+		super().__init__(name, location, next_room, prev_room, current)
 
 class Fire_room(Room):
-	def __init__(self, name, location):
-		super().__init__(name, location)
+	def __init__(self, name, location, next_room, prev_room, current):
+		super().__init__(name, location, next_room, prev_room, current)
 
 class Sezam(Room):
-	def __init__(self, name, location):
-		super().__init__(name, location)
+	def __init__(self, name, location, prev_room, current):
+		super().__init__(name, location, prev_room, current)
 
 class Player(Character):
 	def __init__(self, name, function, location):
@@ -89,6 +111,8 @@ class Gem(Item):
 	def __init__(self, name, function):
 		super().__init__(name, function)
 
+
+
 class Menu:
 	def main_menu(self):
 		print("***********************")
@@ -102,7 +126,10 @@ class Menu:
 		print("3.Exit \n\n")
 
 	def help(self):
-		pass
+		print('''
+		Gra Mysterious Game jest prostą grą tekstową. 
+		
+		''')
 
 	def exit(self):
 		print("\n\n")
@@ -111,8 +138,8 @@ class Menu:
 		quit()
 
 
-
 def main():
+
 	menu = Menu()
 	menu.main_menu()
 	data = input('Your choice is: \n\n')
@@ -129,3 +156,23 @@ def main():
 
 if __name__ == "__main__":
 	main()
+
+'''
+01. Main menu
+	1.Nowa gra
+	2.Pomoc
+	3.Wyjście
+1.1
+	1.Gdzie jestem
+	2.Dostepne przedmioty
+	3.Uzyj item
+	4.Warunki odblokowania DALEJ
+	5.Ostatni pokój( Zabierz Gral) <-- tylko w ostatnim
+	6.Poprzedni pokój
+	7.Następny pokój
+	8.Rozmowa z magiem
+1.2
+	1.O grze i opis
+	2.Sterowanie
+	3.Wyjście
+'''
