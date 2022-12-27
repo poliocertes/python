64aa6cf73e97c713d76e24c2d8d99545a1485e3c
jacek <poliocertes@gmail.com>
2022-12-27 21:35:31 +0100

    27.12.2022-5

--- egzamin_102/main.py
+++ egzamin_102/main.py
@@ -1,14 +1,76 @@
 # Egzamin_102
 import time
 
-rooms = ['Hall','Chamber of magic','Sezam']
+rooms = ['Hall', 'Fire room', 'Water room', 'Sezam']
+
+
+class Menu:
+	def menu(self):
+		print("")
+		print("***********************")
+		print("*     Game Menu       *")
+		print("*********************** \n")
+		print("1.  Current room")
+		print("2.  Items in room")
+		print("3.  Available actions")
+		print("4.  User items")
+		print("5.  Next room")
+		print("6.  Previous room")
+		print("7.  Help")
+		print("8.  Exit \n\n")
+		player_choice = input("Your choice is: ")
+		if int(player_choice) >= 1 and int(player_choice) <= 9:
+			match str(player_choice):
+				case "1":
+					pass
+				case "2":
+					pass
+				case "3":
+					pass
+					pass
+				case "4":
+					print("Player items: ")
+					if len(Hero.show_hero_items()) > 0:
+						for item in Hero.show_hero_items():
+							print(item)
+					else:
+						print("Hero has no items.")
+				case "5":
+					pass
+				case "6":
+					pass
+				case "7":
+					print("About a game")
+					self.help()
+				case "8":
+					time.sleep(1)
+					quit()
+		else:
+			print("You have to choose a correct nummer.")
+			print("Press any key to return.")
+			input()
+			self.menu()
+
+	def help(self):
+		print('''
+
+		Gra Mysterious Game jest prostą grą tekstową. Zadaniem jest przechodzenie między komnatami, znajdowanie przedmiotów i ich użycie.
+		Po osiągnięciu ostatniej komnaty i zobyciu skarbu następuje koniec gry. Przedmioty należy zebrać w odpowiedniej kolejności oraz pilnować poziomu
+		życia tak aby nie spadł on do zera gdyż wykonywanie czynności i zbieranie przedmiotów wiąże się z utratą pewnej porcji energi. 
+		Gracz w każdej chwili może wywołać listę dostępnych opcji poprzez wybranie z menu stosownej pozycji. \n\n
+
+		''')
+		print("Press any key to return")
+		input()
+		self.menu()
+
+
 class Room:
-	def __init__(self, name, current_room):
+	def __init__(self, name):
 		self.name = name
-		self.current_room = current_room
 
 	def current_room(self):
-		return self.current_room
+		print(self.name)
 
 	def next_room(self):
 		pass
@@ -17,86 +79,38 @@ class Room:
 		pass
 
 
-class Hall(Room):
-	pass
-
-
-class Store(Room):
-	pass
-
-
-class Sezam(Room):
-	pass
-
-
 class Character:
-	def __init__(self, name, age, power_level):
+	def __init__(self, name, health, age):
 		self.name = name
+		self.health = health
 		self.age = age
-		self.power_level = power_level
 
 	def introduce_yourself(self):
-		print("Hallo " + self.name + ". You are " + str(self.age) + " years old and you have a " + str(self.power_level) + " points of energy. \n")
+		print("\nHello " + self.name)
+		print("You are " + str(self.age) + " years old. Your health level is now " + str(self.health) + ".")
+		print("Let's play the game. \n")
 
-
-class Player(Character):
-	def __init__(self, name, age, power_level, magic_level):
-		super().__init__(name, age, power_level)
-		self.magic_level = magic_level
-		self.player_items = ['Sword']
-
-	def show_player_items(self):
-		if len(self.player_items):
-			print("\nPlayer items:\n")
-			for item in self.player_items:
-				print(item)
-		else:
-			print("Player has no items.")
-
-	def use_item(self):
-		pass
-
-
-class Mag(Character):
-	def __init__(self):
-		super().__init__()
 
 class Item:
 	pass
 
 
-class Menu:
-	def main_menu(self):
-		print("Let's play the game. ")
-		print("* * * * * * * * * * * * *")
-		print(" * Mysterious game menu  *")
-		print("* * * * * * * * * * * * *")
-		print("1.	Player items")
-		print("2.	Use item")
-		print("3.	Get item")
-		print("4.	Current room")
-		print("5.	Next room")
-		print("6.	Prev room")
-		print("7.	Help")
-		print("8.	Exit")
+class Hero(Character):
+	def __init__(self, name, health, age):
+		super().__init__(name, health, age)
+		self.hero_items = ['Sword']
 
-	def menu_help(self):
-		print('''
+	def show_hero_items(self):
+		print(self.name + '\n items: \n')
+		for item in self.hero_items:
+			print(item)
 
-				Gra Mysterious Game jest prostą grą tekstową. Zadaniem jest przechodzenie między komnatami, znajdowanie przedmiotów i ich użycie.
-				Po osiągnięciu ostatniej komnaty i zobyciu skarbu następuje koniec gry. Przedmioty należy zebrać w odpowiedniej kolejności oraz pilnować poziomu
-				życia tak aby nie spadł on do zera gdyż wykonywanie czynności i zbieranie przedmiotów wiąże się z utratą pewnej porcji energi. 
-				Gracz w każdej chwili może wywołać listę dostępnych opcji poprzez wybranie z menu stosownej pozycji. \n
 
-				''')
-		print("Press any key to return")
-		input()
-		self.main_menu()
-
+class Mag(Character):
+	pass
 
 
 def main():
-	menu = Menu()
 	hero_name = input("Enter your name: ")
 	while not hero_name.isalpha():
 		print("Only letters are allowed")
@@ -104,38 +118,32 @@ def main():
 	hero_age = input("How old are you? ")
 	while not hero_age.isnumeric():
 		print("Enter a number")
-		hero_age = input("Enter again: ")
-	player = Player(hero_name, hero_age, 100, 50)
-	player.introduce_yourself()
-	menu.main_menu()
-	player_choice = input("Your choice: ")
+		hero_age = input("Enter again how old are you: ")
+	hero = Hero(hero_name, 100, hero_age)
+	hero.introduce_yourself()
+	menu = Menu()
+	menu.menu()
+	player_choice = input('Your choice is: ')
 	if 8 >= int(player_choice) >= 1:
 		match str(player_choice):
 			case "1":
-				player.show_player_items()
-				menu.main_menu()
-				input()
+				menu.menu()
 			case "2":
-				pass
+				menu.help()
 			case "3":
-				pass
-			case "4":
-				pass
-			case "5":
-				pass
-			case "":
-				pass
-			case "7":
-				print("Help menu")
-				menu.menu_help()
-			case "8":
-				print("\nYou leave the game.")
-				time.sleep(1)
-				quit()
+				print("\nPress any key to exit.\n")
+				input()
+				print("\n")
 	else:
-		print("Value Error. Please enter correct number. ")
-		menu.main_menu()
+		print("You have to choose a correct nummer.")
+		print("Press any key to return.")
+		input()
+		menu.menu()
 
 
 if __name__ == "__main__":
 	main()
+
+
+
+
