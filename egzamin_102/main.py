a1f15e25c72e7acc7aca599d60f31c6cf8940351
jacek <poliocertes@gmail.com>
2022-12-27 20:28:27 +0100

    27.12.2022-2

--- egzamin_102/main.py
+++ egzamin_102/main.py
@@ -1,5 +1,7 @@
 # Egzamin_102
+import time
 
+rooms = ['Hall','Chamber of magic','Sezam']
 class Room:
 	def __init__(self, name):
 		self.name = name
@@ -34,34 +36,105 @@ class Character:
 		self.power_level = power_level
 
 	def introduce_yourself(self):
-		print(
-			"Hallo " + self.name + ". You are " + str(self.age) + " years old and you have a " + str(self.power_level) + " points of energy. ")
+		print("Hallo " + self.name + ". You are " + str(self.age) + " years old and you have a " + str(self.power_level) + " points of energy. \n")
 
 
 class Player(Character):
-	def __init__(self, name, age, power_level):
+	def __init__(self, name, age, power_level, magic_level):
 		super().__init__(name, age, power_level)
+		self.magic_level = magic_level
+		self.player_items = ['Sword']
+
+	def show_player_items(self):
+		if len(self.player_items):
+			print("\nPlayer items:\n")
+			for item in self.player_items:
+				print(item)
+		else:
+			print("Player has no items.")
+
+	def use_item(self):
+		pass
 
 
 class Mag(Character):
-	pass
-
+	def __init__(self):
+		super().__init__()
 
 class Item:
 	pass
 
 
 class Menu:
-	pass
+	def main_menu(self):
+		print("Let's play the game. ")
+		print("* * * * * * * * * * * * *")
+		print(" * Mysterious game menu  *")
+		print("* * * * * * * * * * * * *")
+		print("1.	Player items")
+		print("2.	Use item")
+		print("3.	Get item")
+		print("4.	Current room")
+		print("5.	Next room")
+		print("6.	Prev room")
+		print("7.	Help")
+		print("8.	Exit")
+
+	def menu_help(self):
+		print('''
+
+				Gra Mysterious Game jest prostą grą tekstową. Zadaniem jest przechodzenie między komnatami, znajdowanie przedmiotów i ich użycie.
+				Po osiągnięciu ostatniej komnaty i zobyciu skarbu następuje koniec gry. Przedmioty należy zebrać w odpowiedniej kolejności oraz pilnować poziomu
+				życia tak aby nie spadł on do zera gdyż wykonywanie czynności i zbieranie przedmiotów wiąże się z utratą pewnej porcji energi. 
+				Gracz w każdej chwili może wywołać listę dostępnych opcji poprzez wybranie z menu stosownej pozycji. \n
+
+				''')
+		print("Press any key to return")
+		input()
+		self.main_menu()
+
 
 
 def main():
+	menu = Menu()
 	hero_name = input("Enter your name: ")
+	while not hero_name.isalpha():
+		print("Only letters are allowed")
+		hero_name = input("Enter a correct player name: ")
 	hero_age = input("How old are you? ")
-	player = Character(hero_name, hero_age, 100)
+	while not hero_age.isnumeric():
+		print("Enter a number")
+		hero_age = input("Enter again: ")
+	player = Player(hero_name, hero_age, 100, 50)
 	player.introduce_yourself()
-	print("Let's play the game. ")
-	choice = input()
+	menu.main_menu()
+	player_choice = input("Your choice: ")
+	if 8 >= int(player_choice) >= 1:
+		match str(player_choice):
+			case "1":
+				player.show_player_items()
+				menu.main_menu()
+				input()
+			case "2":
+				pass
+			case "3":
+				pass
+			case "4":
+				pass
+			case "5":
+				pass
+			case "":
+				pass
+			case "7":
+				print("Help menu")
+				menu.menu_help()
+			case "8":
+				print("\nYou leave the game.")
+				time.sleep(1)
+				quit()
+	else:
+		print("Value Error. Please enter correct number. ")
+		menu.main_menu()
 
 
 if __name__ == "__main__":
