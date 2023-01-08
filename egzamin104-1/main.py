768e6f3048cbaef8170058397cc79a6283962cbc
jacek <poliocertes@gmail.com>
2023-01-08 20:47:57 +0100

    Initial

---
+++ egzamin104-1/main.py
@@ -0,0 +1,61 @@
+import random
+from tkinter import Tk
+
+window = Tk()
+window.geometry('800x600')
+window.title('Papier, Kamień, nożyce')
+window.configure(bg='green')
+
+window.mainloop()
+
+# items = ['papier', 'kamien', 'nozyce']
+# comp_choice = (random.choice(items))
+# game = True
+# print('Gra w Papier, kamien, nozyce')
+#
+# while game:
+# 	print('MENU')
+# 	print('Masz trzy możliwosci: 1 -> Papier, 2 -> Kamien, 3 -> Nozyce')
+# 	user_choice = input("Dokonaj wyboru: \n")
+# 	if int(user_choice) == 1:
+# 		print('Wybrales papier')
+# 		if comp_choice == 'papier':
+# 			print('Komputer wylosował papier')
+# 			print('REMIS')
+# 		elif comp_choice == 'kamien':
+# 			print('Komputer wylosował kamien')
+# 			print('Wygrywa Gracz')
+# 		else:
+# 			print('Komputer wylosował nozyce')
+# 			print('Wygrywa Komputer')
+# 	elif int(user_choice) == 2:
+# 		print('Wybrales kamien')
+# 		if comp_choice == 'papier':
+# 			print('Komputer wylosował papier')
+# 			print('Wygrywa Komputer')
+# 		elif comp_choice == 'kamien':
+# 			print('Komputer wylosował kamien')
+# 			print('Remis')
+# 		else:
+# 			print('Komputer wylosował nozyce')
+# 			print('Wygrywa Gracz')
+# 	elif int(user_choice) == 3 :
+# 		print('Wybrales nozyce')
+# 		if comp_choice == 'papier':
+# 			print('Komputer wylosował papier')
+# 			print('wygrywa Gracz')
+# 		elif comp_choice == 'kamien':
+# 			print('Komputer wylosował kamien')
+# 			print('Wygrywa Komputer')
+# 		else:
+# 			print('Komputer wylosował nozyce')
+# 			print('Remis')
+#
+# 	print('Chcesz dalej grac?')
+# 	print('1 -> TAK, Kązdy inny klawisz: -> NIE:')
+# 	next_step = input("Moj wybor to: ")
+# 	if next_step == '1':
+# 		game = True
+# 	else:
+# 		game = False
+
