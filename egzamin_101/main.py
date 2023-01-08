05c7c411c3597def5e93b7c5301d9868600cf756
jacek <poliocertes@gmail.com>
2023-01-08 20:47:27 +0100

    Fix small error

--- egzamin_101/main.py
+++ egzamin_101/main.py
@@ -1,54 +1,60 @@
 import random
 
+from tkinter import *
 
-items = ['papier', 'kamien', 'nozyce']
-comp_choice = (random.choice(items))
-game = True
-print('Gra w Papier, kamien, nozyce')
+window = Tk()
+window.title('Papier, kamień, nożyce')
+window.geometry('800x600')
+window.mainloop()
 
-while game:
-	print('MENU')
-	print('Masz trzy możliwosci: 1 -> Papier, 2 -> Kamien, 3 -> Nozyce')
-	user_choice = input("Dokonaj wyboru: \n")
-	if int(user_choice) == 1:
-		print('Wybrales papier')
-		if comp_choice == 'papier':
-			print('Komputer wylosował papier')
-			print('REMIS')
-		elif comp_choice == 'kamien':
-			print('Komputer wylosował kamien')
-			print('Wygrywa Gracz')
-		else:
-			print('Komputer wylosował nozyce')
-			print('Wygrywa Komputer')
-	elif int(user_choice) == 2:
-		print('Wybrales kamien')
-		if comp_choice == 'papier':
-			print('Komputer wylosował papier')
-			print('Wygrywa Komputer')
-		elif comp_choice == 'kamien':
-			print('Komputer wylosował kamien')
-			print('Remis')
-		else:
-			print('Komputer wylosował nozyce')
-			print('Wygrywa Gracz')
-	elif int(user_choice) == 3 :
-		print('Wybrales nozyce')
-		if comp_choice == 'papier':
-			print('Komputer wylosował papier')
-			print('wygrywa Gracz')
-		elif comp_choice == 'kamien':
-			print('Komputer wylosował kamien')
-			print('Wygrywa Komputer')
-		else:
-			print('Komputer wylosował nozyce')
-			print('Remis')
-
-	print('Chcesz dalej grac?')
-	print('1 -> TAK, Kązdy inny klawisz: -> NIE:')
-	next_step = input("Moj wybor to: ")
-	if next_step == '1':
-		game = True
-	else:
-		game = False
+# items = ['papier', 'kamien', 'nozyce']
+# comp_choice = (random.choice(items))
+# game = True
+# print('Gra w Papier, kamien, nozyce')
+#
+# while game:
+# 	print('MENU')
+# 	print('Masz trzy możliwosci: 1 -> Papier, 2 -> Kamien, 3 -> Nozyce')
+# 	user_choice = input("Dokonaj wyboru: \n")
+# 	if int(user_choice) == 1:
+# 		print('Wybrales papier')
+# 		if comp_choice == 'papier':
+# 			print('Komputer wylosował papier')
+# 			print('REMIS')
+# 		elif comp_choice == 'kamien':
+# 			print('Komputer wylosował kamien')
+# 			print('Wygrywa Gracz')
+# 		else:
+# 			print('Komputer wylosował nozyce')
+# 			print('Wygrywa Komputer')
+# 	elif int(user_choice) == 2:
+# 		print('Wybrales kamien')
+# 		if comp_choice == 'papier':
+# 			print('Komputer wylosował papier')
+# 			print('Wygrywa Komputer')
+# 		elif comp_choice == 'kamien':
+# 			print('Komputer wylosował kamien')
+# 			print('Remis')
+# 		else:
+# 			print('Komputer wylosował nozyce')
+# 			print('Wygrywa Gracz')
+# 	elif int(user_choice) == 3 :
+# 		print('Wybrales nozyce')
+# 		if comp_choice == 'papier':
+# 			print('Komputer wylosował papier')
+# 			print('wygrywa Gracz')
+# 		elif comp_choice == 'kamien':
+# 			print('Komputer wylosował kamien')
+# 			print('Wygrywa Komputer')
+# 		else:
+# 			print('Komputer wylosował nozyce')
+# 			print('Remis')
+#
+# 	print('Chcesz dalej grac?')
+# 	print('1 -> TAK, Kązdy inny klawisz: -> NIE:')
+# 	next_step = input("Moj wybor to: ")
+# 	if next_step == '1':
+# 		game = True
+# 	else:
+# 		game = False
 
