19a0d3a9a601695a6924372727c659969ca62c10
jacek <poliocertes@gmail.com>
2023-01-14 12:10:42 +0100

    new version

--- egzamin104-1/main.py
+++ egzamin104-1/main.py
@@ -6,26 +6,26 @@ from tkinter import font as tkFont
 
 
 class Bubble_sort:
-	def end_menu(self):
-		print('Co dalej ?')
-		print('1. Kolejne sortowanie ')
-		print('2. Wyjście ')
-		user_input = input()
-		match str(user_input):
-			case '1':
-				main()
+	# def end_menu(self):
+	# 	print('Co dalej ?')
+	# 	print('1. Kolejne sortowanie ')
+	# 	print('2. Wyjście ')
+	# 	user_input = input()
+	# 	match str(user_input):
+	# 		case '1':
+	# 			main()
 
-			case '2':
-				quit()
+	# 		case '2':
+	# 			quit()
 
-			case other:
-				print('Zły wybór. Wracamy do gry.')
-				main()
+	# 		case other:
+	# 			print('Zły wybór. Wracamy do gry.')
+	# 			main()
 
 	def draw(self):
 		window = Tk()
 		window.title('Buble Sort')
-		title_frame = Frame(window, height=200)
+		title_frame = Frame(window, height=200, width=475)
 		window.geometry('600x400')
 		window.resizable(False, False)
 		window.configure(bg='blue')
@@ -39,26 +39,26 @@ def main():
 	bub_sort = Bubble_sort()
 	bub_sort.draw()
 
-	print('Sortowanie babelkowe Python')
-	items_list = []
-	user_input = input('Podaj liczby które chcesz posortować rozdzielając je przecinkiem: \n').split(',')
-	items_list.extend(user_input)
-	if len(items_list) > 1:
-		items_list = [int(x) for x in items_list]
-		list_lenght = len(items_list) - 1
-		print('\nOriginalna lista: ', items_list)
-		for i in range(0, list_lenght):
-			for j in range(0, list_lenght - i):
-				if items_list[j] > items_list[j + 1]:
-					temporary_list_item_value = items_list[j]
-					items_list[j] = items_list[j + 1]
-					items_list[j + 1] = temporary_list_item_value
-		print('Posortowana lista: ', items_list, '\n')
-		bub_sort.end_menu()
-	else:
-		print('Lista jest pusta.')
-		print('Musisz dodać przynajmniej dwie liczby. \n')
-		main()
+	# print('Sortowanie babelkowe Python')
+	# items_list = []
+	# user_input = input('Podaj liczby które chcesz posortować rozdzielając je przecinkiem: \n').split(',')
+	# items_list.extend(user_input)
+	# if len(items_list) > 1:
+	# 	items_list = [int(x) for x in items_list]
+	# 	list_lenght = len(items_list) - 1
+	# 	print('\nOriginalna lista: ', items_list)
+	# 	for i in range(0, list_lenght):
+	# 		for j in range(0, list_lenght - i):
+	# 			if items_list[j] > items_list[j + 1]:
+	# 				temporary_list_item_value = items_list[j]
+	# 				items_list[j] = items_list[j + 1]
+	# 				items_list[j + 1] = temporary_list_item_value
+	# 	print('Posortowana lista: ', items_list, '\n')
+	# 	bub_sort.end_menu()
+	# else:
+	# 	print('Lista jest pusta.')
+	# 	print('Musisz dodać przynajmniej dwie liczby. \n')
+	# 	main()
 
 
 if __name__ == '__main__':
