e6491cd8079e6ae71ea49c29728f1819def0b6db
jacek <poliocertes@gmail.com>
2023-01-25 21:42:33 +0100

    25.01.2023-2

--- Gra/main.py
+++ Gra/main.py
@@ -1,3 +1,5 @@
+from tkinter import Label
+
 import pygame
 import random
 from random import randrange
@@ -50,8 +52,8 @@ class GameBall(object):
     def __init__(self, x, y):
         self.x_cord = x
         self.y_cord = y
-        self.width = 7
-        self.height = 7
+        self.width = 15
+        self.height = 15
         self.color = (255, 140, 0)
         self.x_vel = 2
         self.y_vel = 3
@@ -70,30 +72,29 @@ class GameBall(object):
 class Brick(object):
 
     def __init__(self):
-        self.x_cord = 5
-        self.y_cord = 5
-        self.width = 100
-        self.height = 20
-        self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-        self.rows = 8
+        self.x_cord = 15
+        self.y_cord = 15
+        self.width = 120
+        self.height = 50
+        self.rows = 4
         self.cols = 6
         self.bricks = []
-
+        self.color_list = [(randrange(100, 255), randrange(100, 255), randrange(100, 255)) for i in range(self.cols) for j in
+                      range(self.rows)]
 
     def draw(self):
-        block_list = [pygame.Rect(10 + 120 * i, 10 + 70 * j, 100, 50) for i in range(10) for j in range(4)]
-        color_list = [(randrange(50, 255), randrange(50, 255), randrange(50, 255)) for i in range(10) for j in range(4)]
-        [pygame.draw.rect(screen, color_list[color], block) for color, block in enumerate(block_list)]
-
+        block_list = [pygame.Rect(self.x_cord + (self.width + 10) * i, self.y_cord + (self.height + 10) * j, self.width, self.height) for i in range(self.cols) for j in range(self.rows)]
+        [pygame.draw.rect(screen, self.color_list[color], block) for color, block in enumerate(block_list)]
 
 
 # main function
 def main():
     running = True
     clock = pygame.time.Clock()
-    paddle = Paddle(WIDTH//2 - 50, (HEIGHT - 10), PADDLE_WIDTH, PADDLE_HEIGHT)
-    gameball = GameBall(WIDTH // 2, HEIGHT - 55)
+    paddle = Paddle(WIDTH//2 - 50, (HEIGHT - 100), PADDLE_WIDTH, PADDLE_HEIGHT)
+    gameball = GameBall(WIDTH // 2, HEIGHT - 125)
     lives = 5
+    score = 12
     brick = Brick()
     while running:  # main loop
         # screen.blit(bg, (0, 0))
@@ -103,7 +104,9 @@ def main():
         gameball.draw()
         gameball.move()
         brick.draw()
-
+        pygame.draw.rect(screen, 'white', pygame.Rect(0, 740, 800, 5))
+        score = pygame.font.Font.render(pygame.font.SysFont('Arial Bold', 40), f'Score: {score}', True, (255, 255, 255))
+        lives = pygame.font.Font.render(pygame.font.SysFont('Arial Bold', 40), f'Lives: {lives}', True, (255, 255, 255))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -116,7 +119,7 @@ def main():
         if keys[pygame.K_RIGHT]:
             paddle.move_right()
 
-        if gameball.y_cord >= HEIGHT:
+        if gameball.y_cord >= HEIGHT - 65:
             gameball.y_vel *= -1
         if gameball.y_cord <= 2:
             gameball.y_vel *= -1
@@ -127,9 +130,15 @@ def main():
         if gameball.x_cord <= 1:
             gameball.x_vel *= -1
 
-        if paddle.x_cord + paddle.width >= gameball.x_cord > paddle.x_cord and paddle.y_cord < gameball.y_cord <= paddle.y_cord + paddle.height:
-            gameball.y_vel *= -1
+        for j in range(brick.cols):
+            for i in range(brick.rows):
+                if gameball.y_cord < brick.x_cord + brick.height:
+                    print('ttt')
 
+        if paddle.x_cord + paddle.width >= gameball.x_cord > paddle.x_cord and paddle.y_cord < gameball.y_cord +gameball.height:
+            gameball.y_vel *= -1
+        screen.blit(score, (15, 760))
+        screen.blit(lives, (650, 760))
         pygame.display.update()
 
 
