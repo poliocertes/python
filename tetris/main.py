6880784a73d8d6c9ff03e4668b9515eec7195e6d
jacek <poliocertes@gmail.com>
2023-01-28 22:59:23 +0100

    Initial commit

---
+++ tetris/main.py
@@ -0,0 +1,30 @@
+import pygame
+import random
+
+colors = ['red', 'blue', 'green', 'orange', 'black']
+blocks = []
+score = 0
+
+
+class GameBoard:
+	def __init__(self):
+		pass
+
+
+class Block:
+	def __init__(self):
+		pass
+
+	def rotate(self):
+		pass
+
+	def move(self):
+		pass
+
+
+def main():
+	print('Tetris')
+
+
+if __name__ == '__main__':
+	main()
