8a519fd1afc141fc7907ff14c9943d74d09283a0
jacek <poliocertes@gmail.com>
2023-01-29 07:42:12 +0100

    grid added - finish

---
+++ tetris/classes/gameboard.py
@@ -0,0 +1,14 @@
+# gameboard class
+
+import pygame as pg
+
+class GameBoard:
+    def __init__(self):
+        self.rows = 10
+        self.cols = 20
+        self.block_size = 50
+
+    def draw_grid(self):
+        for i in range(self.cols):
+            for j in range(self.rows):
+                pg.draw.rect(screen, 'black', (j * self.block_size, i * self.block_size, 50, 50), 1)
--- tetris/main.py
+++ tetris/main.py
@@ -1,28 +1,18 @@
-import pygame
-import random
+from settings import *
+from classes.gameboard import GameBoard
+import pygame as pg
 
-pygame.init()
 
-HEIGHT = 800
-WIDTH = 500
+# pygame init
+pg.init()
+
+# global variables
+
 colors = ['red', 'blue', 'green', 'orange', 'black']
 blocks = []
 score = 0
-screen = pygame.display.set_mode([WIDTH, HEIGHT])
-pygame.display.set_caption('Tetris')
-
-
-class GameBoard:
-	def __init__(self, x, y, width, height):
-		self.x = x
-		self.y = y
-		self.width = width
-		self.height = height
-		self.rows = 8
-		self.cols = 5
-
-	def draw_board(self):
-		pass
+screen = pg.display.set_mode([WINDOW_WIDTH, WINDOW_HEIGHT])
+pg.display.set_caption('Tetris')
 
 
 class Block(GameBoard):
@@ -39,38 +29,37 @@ class Block(GameBoard):
 
 def main():
 	running = True
+	game_board = GameBoard()
 
 	while running:
 		screen.fill('blue')
-		show_score = pygame.font.Font.render(pygame.font.SysFont('Arial', 30), f'Score: {score}', True, (255, 255, 255))
-		pygame.draw.rect(screen, 'white', pygame.Rect(0, 750, 500, 1))
-		pygame.draw.line(screen, 'white',(0, 50), (500, 50))
-		pygame.draw.line(screen, 'white',(0, 100), (500, 100))
-		pygame.draw.line(screen, 'white',(0, 150), (500, 150))
-		pygame.draw.line(screen, 'white',(0, 200), (500, 200))
-		pygame.draw.line(screen, 'white',(0, 250), (500, 250))
-		pygame.draw.line(screen, 'white',(0, 300), (500, 300))
-		pygame.draw.line(screen, 'white',(0, 350), (500, 350))
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
+		game_board.draw_grid()
+		show_title = pg.font.Font.render(pg.font.SysFont('Arial', 30), 'TETRIS', True, (255, 255, 255))
+		show_score = pg.font.Font.render(pg.font.SysFont('Arial', 30), f'Score: {score}', True, (255, 255, 255))
+		show_next = pg.font.Font.render(pg.font.SysFont('Arial', 30), 'Next', True, (255, 255, 255))
+
+		for event in pg.event.get():
+			if event.type == pg.QUIT:
 				running = False
 				break
-		keys = pygame.key.get_pressed()
+		keys = pg.key.get_pressed()
 
-		if keys[pygame.K_UP]:
+		if keys[pg.K_UP]:
 			print('UP')
 
-		if keys[pygame.K_DOWN]:
+		if keys[pg.K_DOWN]:
 			print('DOWN')
 
-		if keys[pygame.K_LEFT]:
+		if keys[pg.K_LEFT]:
 			print('LEFT')
 
-		if keys[pygame.K_RIGHT]:
+		if keys[pg.K_RIGHT]:
 			print('RIGHT')
 
-		screen.blit(show_score, (15, 760))
-		pygame.display.update()
+		screen.blit(show_title, (365, 25))
+		# screen.blit(show_score, (15, 25))
+		# screen.blit(show_next, (675, 25))
+		pg.display.update()
 
 
 if __name__ == '__main__':
