1e95521f8545831be3c43d99f2b4c59ead4f90fd
jacek <poliocertes@gmail.com>
2023-01-30 18:36:18 +0100

    startet to create blocks - in progress

--- tetris/classes/gameboard.py
+++ tetris/classes/gameboard.py
@@ -1,16 +1,19 @@
 # gameboard class
 
 import pygame as pg
+from settings import *
 
 
 class GameBoard:
     def __init__(self):
         self.rows = 10
         self.cols = 20
-        self.block_size = 50
+        self.block_size = BLOCK_SIZE
 
     def draw_grid(self):
         from main import screen
         for i in range(self.cols):
             for j in range(self.rows):
                 pg.draw.rect(screen, 'black', (j * self.block_size, i * self.block_size, 50, 50), 1)
+
+
--- tetris/main.py
+++ tetris/main.py
@@ -1,18 +1,17 @@
 import pygame.image
+import pygame as pg
 
 from settings import *
 from classes.gameboard import GameBoard
 from classes.block import Block
-import pygame as pg
-
 
 # pygame init
 pg.init()
 
 # global variables
 
-colors = ['red', 'blue', 'green', 'orange', 'black']
-blocks = []
+blocks = [S, Z, I, O, J, L, T]
+blocks_colors = [BLUE, GREEN, RED, YELLOW, ORANGE, BLACK, PINK]
 score = 0
 screen = pg.display.set_mode([WINDOW_WIDTH, WINDOW_HEIGHT])
 tetris_logo = pygame.image.load('assets/tetris.png').convert()
@@ -20,41 +19,41 @@ pg.display.set_caption('Tetris')
 
 
 def main():
-	running = True
-	game_board = GameBoard()
-	block = Block()
+    running = True
+    game_board = GameBoard()
+    block = Block()
 
-	while running:
-		screen.fill(BLUE)
-		game_board.draw_grid()
+    while running:
+        screen.fill(BLUE)
+        game_board.draw_grid()
 
-		show_score = pg.font.Font.render(pg.font.SysFont('Arial', 30), f'Score: {score}', True, (255, 255, 255))
-		show_next = pg.font.Font.render(pg.font.SysFont('Arial', 30), 'Next', True, (255, 255, 255))
+        show_score = pg.font.Font.render(pg.font.SysFont('Arial', 30), f'Score: {score}', True, (255, 255, 255))
+        show_next = pg.font.Font.render(pg.font.SysFont('Arial', 30), 'Next element', True, (255, 255, 255))
 
-		for event in pg.event.get():
-			if event.type == pg.QUIT:
-				running = False
-				break
-		keys = pg.key.get_pressed()
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+                break
+        keys = pg.key.get_pressed()
 
-		if keys[pg.K_UP]:
-			print('UP')
+        if keys[pg.K_UP]:
+            print('UP')
 
-		if keys[pg.K_DOWN]:
-			print('DOWN')
+        if keys[pg.K_DOWN]:
+            print('DOWN')
 
-		if keys[pg.K_LEFT]:
-			print('LEFT')
+        if keys[pg.K_LEFT]:
+            print('LEFT')
 
-		if keys[pg.K_RIGHT]:
-			print('RIGHT')
+        if keys[pg.K_RIGHT]:
+            print('RIGHT')
 
-		screen.blit(tetris_logo, (600, 50))
-		screen.blit(show_next, (665, 350))
-		screen.blit(show_score, (650, 700))
+        screen.blit(tetris_logo, (600, 50))
+        screen.blit(show_next, (615, 350))
+        screen.blit(show_score, (650, 700))
 
-		pg.display.update()
+        pg.display.update()
 
 
 if __name__ == '__main__':
-	main()
+    main()
