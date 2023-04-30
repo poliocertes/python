bd520a743671f4c9f85109583ff72c133620c8a7
Jacek Jablonski <poliocertes@gmail.com>
2023-04-30 20:54:29 +0200

    Error fix

--- KiK/classes/game.py
+++ KiK/classes/game.py
@@ -18,38 +18,41 @@ class Game:
             if event.type == pg.QUIT:
                 pg.quit()
                 sys.exit()
-            if event.type == pg.MOUSEBUTTONDOWN:  
+            if event.type == pg.MOUSEBUTTONDOWN:
                 current_pos = pg.mouse.get_pos()
                 if current_pos[0] <= 180 and current_pos[1] < 180:
                     print('Field nr 1')
-                elif 255 >= current_pos[0] > 180 > current_pos[1]:
-                    print('Field nr 2')
-                elif 255 < current_pos[0] < 330 and current_pos[1] < 180:
-                    print('Field nr 3')
-                elif current_pos[0] < 180 < current_pos[1] < 255:
-                    print('Field nr 4')
-                elif 180 < current_pos[0] < 255 and 180 < current_pos[1] < 255:
-                    print('Field nr 5')
-                elif 330 > current_pos[0] > 255 > current_pos[1] > 180:
-                    print('Field nr 6')
-                elif current_pos[0] < 180 and 255 < current_pos[1] < 330:
-                    print('Field nr 7')
-                elif 180 < current_pos[0] < 255 and current_pos[1] < 330:
-                    print('Field nr 8')
-                elif 255 < current_pos[0] < 330 and 255 < current_pos[1] < 330:
-                    print('Field nr 9')
+                    self.tictactoe.mark_field_one()
+            #     elif 255 >= current_pos[0] > 180 > current_pos[1]:
+            #         print('Field nr 2')
+            #     elif 255 < current_pos[0] < 330 and current_pos[1] < 180:
+            #         print('Field nr 3')
+            #     elif current_pos[0] < 180 < current_pos[1] < 255:
+            #         print('Field nr 4')
+            #     elif 180 < current_pos[0] < 255 and 180 < current_pos[1] < 255:
+            #         print('Field nr 5')
+            #     elif 330 > current_pos[0] > 255 > current_pos[1] > 180:
+            #         print('Field nr 6')
+            #     elif current_pos[0] < 180 and 255 < current_pos[1] < 330:
+            #         print('Field nr 7')
+            #     elif 180 < current_pos[0] < 255 and current_pos[1] < 330:
+            #         print('Field nr 8')
+            #     elif 255 < current_pos[0] < 330 and 255 < current_pos[1] < 330:
+            #         print('Field nr 9')
 
     def screen_update(self):
         self.clock.tick(FPS)
 
     def game(self):
         self.screen.fill(color=BOARD_COLOR)
-        self.tictactoe.draw()
         self.check_events()
+        self.tictactoe.draw()
         pg.display.update()
 
     def run(self):
         while True:
             self.game()
-            self.screen_update()
+            # self.screen_update()
+
+
             
--- KiK/classes/tictactoe.py
+++ KiK/classes/tictactoe.py
@@ -3,15 +3,6 @@
 from classes.settings import *
 
 
-class Symbol:
-	def __init__(self):
-		self.image = pg.Surface([BLOCK_SIZE, BLOCK_SIZE])
-		self.symbols = []
-
-	def draw(self):
-		pass
-
-
 class Tictactoe:
 	def __init__(self, game):
 		self.game = game
@@ -21,14 +12,13 @@ class Tictactoe:
 		for i in range(COLS):
 			for j in range(ROWS):
 				pg.draw.rect(self.game.screen, 'black', (j * BLOCK_SIZE + (HEIGHT/4), i * BLOCK_SIZE + (WIDTH/4), BLOCK_SIZE, BLOCK_SIZE), 1)
-				# pg.draw.rect(self.game.screen, '#114b00', (WIDTH / 4, HEIGHT / 4, BLOCK_SIZE, BLOCK_SIZE))
-				
-	def mark_field(self):
-		pg.draw.rect(self.game.screen, '#114b00', (WIDTH/4, HEIGHT/4, BLOCK_SIZE, BLOCK_SIZE))
-		pg.display.update()
+
+	def mark_field_one(self):
+		pg.draw.rect(self.game.screen, 'red', (WIDTH/4, HEIGHT/4, BLOCK_SIZE, BLOCK_SIZE))
 
 	def update(self):
 		pass
 
 	def draw(self):
 		self.draw_grid()
+
